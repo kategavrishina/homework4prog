d1c962bd53c6672ee1288143a8a2ab63cf9fc62f
kategavrishina <31881632+kategavrishina@users.noreply.github.com>
2017-11-19 22:05:22 +0300

    Update 5.py

--- bonus/5.py
+++ bonus/5.py
@@ -1,10 +1,15 @@
 cons = "bcdfghjklmnpqrstvwxz"
+rus = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"
 while 1:
     print ()
     word = input ("Введите слово на английском: ")
     for i in word:
-        if i in cons:
-            i = i + "aig"
-            print (i, end = "")
+        if i in rus:
+            print ("Слово на русском, исправьте.")
+            break
         else:
-            print (i, end = "")
+            if i in cons:
+                i = i + "aig"
+                print (i, end = "")
+            else:
+                print (i, end = "")
