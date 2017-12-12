89aa68598170837b9acbce059a166b8110ed97e3
kategavrishina <31881632+kategavrishina@users.noreply.github.com>
2017-12-12 13:18:10 +0300

    First two

---
+++ control/control1.py
@@ -0,0 +1,16 @@
+#with open("Extinct_languages.tsv", encoding="utf-8") as f:
+with open("new1.txt", encoding="utf-8") as f:
+    text = f.readlines()
+    #1
+    for line in text:
+        l = len(line)
+        if l > 35:
+            print(line, "(",l,")", sep = '')
+    #2
+    num = 0
+    for line in text:
+        if "Critically endangered" in line:
+            num += 1
+    print(num)
+
+            
