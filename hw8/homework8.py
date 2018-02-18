36661231b79b3c79b1209c69860a1d98ca9172b3
kategavrishina <31881632+kategavrishina@users.noreply.github.com>
2018-02-18 23:23:48 +0300

    Add files via upload

---
+++ hw8/homework8.py
@@ -0,0 +1,41 @@
+import random
+
+def words(filename):
+    d = {}
+    with open(filename,'r',encoding = 'utf-8') as f:
+        for line in f:
+            word, d[word] = line.strip().split(',')
+    return d
+
+def game(d):
+    y = []
+    n = []
+    with open('true.txt','r',encoding = 'utf-8') as yes:
+        for line in yes:
+            y.append(line.strip())
+    with open('false.txt','r',encoding = 'utf-8') as no:
+        for line in no:
+            n.append(line.strip())
+    print('Вам будет предложена подсказка в виде прилагательного,\nк которому надо подобрать существительное.\n')
+    start = input('Если хотите сыграть, нажмите Enter.')
+    #while start == '':
+    if start == '':
+        key = random.choice(list(d))
+        print(d[key], '...')
+        answer = input('Ваш ответ: ')
+        if answer.lower() == key.lower():
+            print(random.choice(y), '\n')
+            start = input('Если хотите сыграть, нажмите Enter.')
+        else:
+            #while answer.lower() != key.lower():
+                print(random.choice(n))
+                answer = input('Ваш ответ: ')
+                if answer.lower() == key.lower():
+                    print(random.choice(y), '\n')
+                    start = input('Если хотите сыграть, нажмите Enter.')
+
+def main():
+    print(game(words('words.csv')))
+
+if __name__ == '__main__':
+    main()
