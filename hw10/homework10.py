31787789ee8af5ecb7ea82991c7a573efa52c74f
kategavrishina <31881632+kategavrishina@users.noreply.github.com>
2018-04-02 12:09:45 +0300

    Add files via upload

---
+++ hw10/homework10.py
@@ -0,0 +1,17 @@
+import re
+
+def searching():
+    filename = input('Введите название файла с расширением: ')
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            r = re.search('data-wikidata-external-id="(...)" data-wikidata-property-id="P220"', line)
+            if r:
+                string = r.group(1)
+                with open('result.txt', 'a', encoding = 'utf-8') as g:
+                    g.write(string + '\n')
+    
+def main():
+    print(searching())
+
+if __name__=='__main__':
+    main()
