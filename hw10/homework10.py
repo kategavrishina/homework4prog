36076aa6fa89f6c7f73d26747906be2a05891ff7
kategavrishina <31881632+kategavrishina@users.noreply.github.com>
2018-04-02 20:52:47 +0300

    Update homework10.py

--- hw10/homework10.py
+++ hw10/homework10.py
@@ -1,17 +1,17 @@
 #Вариант 7
-#Прости, что не прикладываю сайты, вместе с ними скачивается множество других файлов, прилагающихся к HTML
 
 import re
 
 def searching():
     filename = input('Введите название файла с расширением: ')
     with open(filename, 'r', encoding = 'utf-8') as f:
-        for line in f:
-            r = re.search('data-wikidata-external-id="(...)" data-wikidata-property-id="P220"', line)
-            if r:
-                string = r.group(1)
-                with open('result.txt', 'a', encoding = 'utf-8') as g:
-                    g.write(string + '\n')
+        text = f.read()
+        r = re.search('ISO 639-3.+\n.+data-wikidata-external-id="(...)"', text)
+        if r:
+            string = r.group(1)
+            print(string)
+            with open('result.txt', 'a', encoding = 'utf-8') as g:
+                g.write(string + '\n')
     
 def main():
     print(searching())
