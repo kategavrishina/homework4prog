4915ffc39433f5554b322ce4a026e48b4f66506c
kategavrishina <31881632+kategavrishina@users.noreply.github.com>
2018-04-02 21:06:03 +0300

    Classwork(17.03)

---
+++ cw_regular/data.py
@@ -0,0 +1,23 @@
+import re
+
+def sentences(filename):
+    s = []
+    with open(filename, encoding='utf-8') as f:
+        text = f.read()
+        sents = text.split('.\n')
+        for sent in sents:
+            s.append(sent)
+    return s
+
+def searching(a):
+    for sent in a:
+        r = re.search('(18[0-9][0-9])|([12]?[1-9] (ян|фев|ма|апреля|авгу|ию|сент|окт|ноя|дек)[а-я]+ [12][0-9][0-9][0-9])', sent)
+        if r:
+            string = r.group()
+            print(string)
+
+def main():
+    print(searching(sentences('war.txt')))
+
+if __name__=='__main__':
+    main()
